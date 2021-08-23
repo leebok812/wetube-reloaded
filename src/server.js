91be9f7e49ebb6ed93fe43024e982a07fedf77aa
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-23 14:20:12 +0900

    INTRODUCTION TO EXPRESS - Recap

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const app = express();
 
 const handleHome = (req, res) => {
     // return  res.end(); 서버에서 사용자로 보낼것이 없이 끝냄
-      return res.send("I still love you.");
+      return res.send("<h1>I still love you.</h1>"); // HTML 보내기 가능. 가능하지만 추천 x
 };
 
 const handleLogin = (req, res) => {
-    return res.send("Login here.");
+    return res.send({ message : "Login here."}); // JSON 보내기 가능
 }
 
 // 아래와 같은 의미 app.get("/", () => console.log("Somebody is trying to go home.")) // 반드시 함수를 보내야 실행됨.

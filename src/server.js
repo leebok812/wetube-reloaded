68c13ec5a81df19a2be8acbc64ec1ac161e0bd14
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-23 13:49:32 +0900

    INTRODUCTION TO EXPRESS - GET Requests

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+// 아래와 같은 의미 app.get("/", () => console.log("Somebody is trying to go home.")) // 반드시 함수를 보내야 실행됨.
+ app.get("/", handleHome) // 반드시 함수를 보내야 실행됨.
+
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}🚀`);
 
 app.listen(PORT, handleListening);

48696863da4dc0694d40d83adef75b7f4b34cef7
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-25 16:57:34 +0900

    Routers - Exports

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {join} from "../controllers/userController";
-import {trending} from "../trends/videoController";
+import {trending} from "../controllers/videoController";
 
 const globalRouter = express.Router();
 

4050b56c9152e25c03e22e4046a457e278aa8d2d
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-26 11:47:07 +0900

    Routers - URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import {see, edit, upload, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload",upload); // 순서 중요! express가 upload가 id라고 인식하지 않기 위해
-videoRouter.get("/:id", see)
-videoRouter.get("/:id/edit", edit)
-videoRouter.get("/:id/delete", deleteVideo)
+videoRouter.get("/:id(\\d+)", see) // 숫자만 들어갈수 있게 (\\d+) 정규식을 적어줌
+videoRouter.get("/:id(\\d+)/edit", edit)
+videoRouter.get("/:id(\\d+)/delete", deleteVideo)
+videoRouter.get("/upload",upload); // 순서 중요! express가 upload가 id라고 인식하지 않기 위해 + 추가 하지만 이제는 순서에 상관없이 쓸수 있음 위에 정규식을 추가하여 숫자만 인식하도록 하였기에
 
 export default videoRouter;

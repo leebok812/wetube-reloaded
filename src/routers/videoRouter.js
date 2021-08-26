e8831a1045ab888184001d67039fb3f0423ad668
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-26 11:32:48 +0900

    Routers - URL Parameters part One

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,7 @@ import {see, edit, upload, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload",upload);
+videoRouter.get("/upload",upload); // 순서 중요! express가 upload가 id라고 인식하지 않기 위해
 videoRouter.get("/:id", see)
 videoRouter.get("/:id/edit", edit)
 videoRouter.get("/:id/delete", deleteVideo)

2f5004abffbe5417275a8128f7bc9c7b7382b68b
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-27 16:22:57 +0900

    Templates - Conditional

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"}); // pageTile comes from your controller
-export const see = (req, res) => res.render("watch"); // 파일명은 띄어쓰기가 있으면 안된다.
- 
+const fakeUser = {
+    username:"JUNE",
+    loggedIn:false
+}
+
+
+
+export const trending = (req, res) => res.render("home", {pageTitle: "Home",fakeUser:fakeUser}); // pageTile comes from your controller
+export const see = (req, res) => res.render("watch"); // 파일명은 띄어쓰기가 있으면 안된다. 
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
